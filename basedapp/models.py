225aa2e300f60573ca7562a385f6d91672ac5624
Sibivan <sibivan0@gmail.com>
2024-10-08 16:06:04 +0700

    error

--- basedapp/models.py
+++ basedapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+'''
 class Instrument(models.Model):
 	cover = models.ImageField(upload_to='images/')
 	types=models.CharField(max_length=50)
@@ -8,6 +9,6 @@ class Instrument(models.Model):
 	isonbase=models.BooleanField(default=False)
 	medonbox=models.BooleanField(default=False)
 	caponbox=models.BooleanField(default=False)
-	stronbox=models.BooleanField(default=False)
+	stronbox=models.BooleanField(default=False)'''
 
 
